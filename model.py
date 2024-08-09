7518be787ddb48206df1935b2d595121ed4c9ab8
Sandeep <86227290+csandeep98@users.noreply.github.com>
2024-08-08 21:36:31 -0400

    added the required libraries

---
+++ model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+import torch.nn as nn
+import torch.functional as F
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
