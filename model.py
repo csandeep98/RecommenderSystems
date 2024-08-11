c5459edd08f17a2e07736979c7204c4024d9cc45
Sandeep <86227290+csandeep98@users.noreply.github.com>
2024-08-10 23:13:43 -0400

    just tryimg to continue my push towards one commit each day

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import math
 import torch.nn as nn
 import torch.functional as F
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardaccakldlkmScaler
 from sklearn.model_selection import train_test_split
 
 # for the transformer architecture we need some functions
@@ -21,6 +21,9 @@ class Transformer:
 class Encoder(nn.Module):
     def __init__(self, params):
         super(Encoder, self).__init__()
+        self.input = input_dim
+        self.output_dim = output_dimg
+        self.activation = nn.sigmoid()
 
         return
 
